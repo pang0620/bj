2fde4c836b5e4d8d3a6ce33f3429f1a1142aadaf
pang <129kjy@gmail.com>
2025-04-09 13:33:41 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[51];
+    scanf("%s", input);
+    printf("%s??!", input);
+    
+    return 0;
+}
