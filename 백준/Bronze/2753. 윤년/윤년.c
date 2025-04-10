8864c2e62d267ec9e61b1c2b581576e3f3bdf5e6
pang <129kjy@gmail.com>
2025-04-10 10:47:48 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    scanf("%d", &year);
+    if ((year%4==0 &&  year%100!=0) || year%400==0) { printf("%d\n", 1); }
+    else { printf("%d\n", 0); }
+    
+    return 0;
+}
